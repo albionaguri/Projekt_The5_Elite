50f256796d59fe90b6e64a69f8cb3b4dc42d1c0e
albionaguri <51866742+albionaguri@users.noreply.github.com>
2020-05-24 18:35:45 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
+
 <?php include("includes/config.php"); ?>
 <?php
 
@@ -7,6 +8,33 @@ function Confirm_Login(){
   }  else {
     $_SESSION["ErrorMessage"]="Login Required !";
     header("location: index.php");
+  }
+}
+
+function checkUsernameExistsOrNot($username){
+  global $con;
+  $sql = "SELECT * FROM users WHERE username = '$username'";
+  $result = mysqli_query($con, $sql);
+
+  if (mysqli_num_rows($result) == 1){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+
+function loginAttempt($username, $password, $role){
+  $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password' AND role = '$role'";
+  $result = mysqli_query($con, $sql);
+
+  if ($row = mysqli_num_rows($result) > 0){
+    return true;
+  }
+  else{
+    return false;
   }
 }
 
