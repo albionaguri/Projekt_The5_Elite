8fcc6ad981aa652ead232e848e64a915a4c5a3eb
albionaguri <51866742+albionaguri@users.noreply.github.com>
2020-05-25 17:00:19 +0200

    Add files via upload

---
+++ delete-teacher.php
@@ -0,0 +1,10 @@
+<?php
+$con = mysqli_connect('localhost','root',"",'srms');
+$Delete_Record_Id = $_GET['Delete'];
+
+$delete_query = "DELETE FROM users WHERE id = '$Delete_Record_Id'";
+$result = mysqli_query($con,$delete_query);
+if($result){
+  header("Location:". "manage-teachers.php");
+}
+ ?>
