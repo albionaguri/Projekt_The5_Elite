0e866e411bf32cbe31591b22161fb3fa2faea297
albionaguri <51866742+albionaguri@users.noreply.github.com>
2020-05-25 18:12:50 +0200

    Add files via upload

---
+++ add-teachers.php
@@ -0,0 +1,198 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+include('includes/sessions.php');
+include('includes/functions.php');
+
+
+if(isset($_POST['submit']))
+{
+  $username=$_POST['username'];
+  $password = md5($_POST['password']);
+  $email=$_POST['email'];
+  $gender=$_POST['gender'];
+  $dob=$_POST['dob'];
+  $roli = "Teacher";
+
+  if (empty($username) || empty($password) ||  empty($email) || empty($gender) || empty($dob)){
+      $_SESSION["ErrorMessage"] = "All fields must be filled out!";
+      header("location: add-teachers.php");
+      exit;
+    }
+
+  if(!empty($email)){
+    if(!strpos($email, "@fshn.edu.al")) {
+      $_SESSION["ErrorMessage"] = "Invalid Email format!";
+      header("location: add-teachers.php");
+      exit;
+    }
+  }
+
+  $sql = "SELECT * FROM users WHERE username='$username'";
+  $res = mysqli_query($con, $sql);
+  $nr = mysqli_num_rows($res);
+  if ($nr == 1){
+      $_SESSION["ErrorMessage"] = "Username is taken!Please try another one!";
+      header("location: add-teachers.php");
+      exit;
+  }
+
+  $sql="INSERT INTO users(username,email, gender, DOB, role,password)
+        VALUES('$username','$email','$gender','$dob', '$roli','$password')";
+  $result = mysqli_query($con, $sql);
+
+  $lastInsertId = mysqli_insert_id($con);
+  if($result){
+    $msg="Teacher added successfully";
+  }
+  else{
+    $error="Something went wrong. Please try again";
+  }
+}
+
+?>
+
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SMS Admin| Teacher Admission< </title>
+  <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+  <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+  <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
+  <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" >
+  <link rel="stylesheet" href="css/prism/prism.css" media="screen" >
+  <link rel="stylesheet" href="css/select2/select2.min.css" >
+  <link rel="stylesheet" href="css/main.css" media="screen" >
+  <script src="js/modernizr/modernizr.min.js"></script>
+</head>
+<body class="top-navbar-fixed">
+  <div class="main-wrapper">
+
+    <?php include('includes/topbar.php');?>
+    <div class="content-wrapper">
+      <div class="content-container">
+
+        <?php include('includes/leftbar.php');?>
+
+        <div class="main-page">
+
+          <div class="container-fluid">
+            <div class="row page-title-div">
+              <div class="col-md-6">
+                <h2 class="title">Teacher Admission</h2>
+              </div>
+            </div>
+
+            <?php
+            echo ErrorMessage();
+             ?>
+
+            <div class="row breadcrumb-div">
+              <div class="col-md-6">
+                <ul class="breadcrumb">
+                  <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
+
+                  <li class="active">Teacher Admission</li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+          <div class="container-fluid">
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="panel">
+                  <div class="panel-heading">
+                    <div class="panel-title">
+                      <h5>Fill the Teacher info</h5>
+                    </div>
+                  </div>
+                  <div class="panel-body">
+                    <?php if($msg){?>
+                      <div class="alert alert-success left-icon-alert" role="alert">
+                        <strong>Well done!</strong><?php echo htmlentities($msg); ?>
+                      </div><?php }
+                      else if($error){?>
+                        <div class="alert alert-danger left-icon-alert" role="alert">
+                          <strong>Oh snap!</strong> <?php echo htmlentities($error); ?>
+                        </div>
+                      <?php } ?>
+                      <form class="form-horizontal" method="post">
+
+                        <div class="form-group">
+                          <label for="default" class="col-sm-2 control-label">Username</label>
+                          <div class="col-sm-10">
+                            <input type="text" name="username" class="form-control" id="fullname"  autocomplete="off">
+                          </div>
+                        </div>
+
+                        <div class="form-group">
+                          <label for="default" class="col-sm-2 control-label">Email</label>
+                          <div class="col-sm-10">
+                            <input type="email" name="email" class="form-control" id="email"  autocomplete="off">
+                          </div>
+                        </div>
+
+                        <div class="form-group">
+                          <label for="default" class="col-sm-2 control-label">Password</label>
+                          <div class="col-sm-10">
+                            <input type="password" name="password" class="form-control" id="password"  autocomplete="off">
+                          </div>
+                        </div>
+
+                        <div class="form-group">
+                          <label for="default" class="col-sm-2 control-label">Gender</label>
+                          <div class="col-sm-10">
+                            <input type="radio" name="gender" value="Male"  checked="">Male
+                            <input type="radio" name="gender" value="Female" >Female
+                          </div>
+                        </div>
+
+
+                            <div class="form-group">
+                              <label for="date" class="col-sm-2 control-label">DOB</label>
+                              <div class="col-sm-10">
+                                <input type="date"  name="dob" class="form-control" id="date">
+                              </div>
+                            </div>
+
+                            <div class="form-group">
+                              <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" name="submit" class="btn btn-primary">Add Teacher</button>
+                              </div>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <script src="js/jquery/jquery-2.2.4.min.js"></script>
+          <script src="js/bootstrap/bootstrap.min.js"></script>
+          <script src="js/pace/pace.min.js"></script>
+          <script src="js/lobipanel/lobipanel.min.js"></script>
+          <script src="js/iscroll/iscroll.js"></script>
+          <script src="js/prism/prism.js"></script>
+          <script src="js/select2/select2.min.js"></script>
+          <script src="js/main.js"></script>
+          <script>
+          $(function($) {
+            $(".js-states").select2();
+            $(".js-states-limit").select2({
+              maximumSelectionLength: 2
+            });
+            $(".js-states-hide").select2({
+              minimumResultsForSearch: Infinity
+            });
+          });
+          </script>
+        </body>
+        </html>
